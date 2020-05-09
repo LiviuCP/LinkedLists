0c1f13bf86a4d8b06c69bb55330f13ce8b9342c4
Liviu Constantin POPA <liviucst@gmail.com>
2020-05-09 09:47:25 +0300

    Updated indentation of a prio queue test

--- Tests/PriorityQueueTests/tst_priorityqueuetests.cpp
+++ Tests/PriorityQueueTests/tst_priorityqueuetests.cpp
@@ -30,74 +30,74 @@ PriorityQueueTests::~PriorityQueueTests()
 
 void PriorityQueueTests::testElementsInsertionDeletion()
 {
-     PriorityQueue* queue = createPriorityQueue();
+    PriorityQueue* queue = createPriorityQueue();
 
-     QVERIFY(isEmptyQueue(queue));
+    QVERIFY(isEmptyQueue(queue));
 
-     // first insert
-     insertIntoPriorityQueue(queue, 4, createObject("Point", createPointPayload(4, 5)));
+    // first insert
+    insertIntoPriorityQueue(queue, 4, createObject("Point", createPointPayload(4, 5)));
 
-     QVERIFY2(!isEmptyQueue(queue), "The queue is empty, no element has been inserted");
+    QVERIFY2(!isEmptyQueue(queue), "The queue is empty, no element has been inserted");
 
-     insertIntoPriorityQueue(queue, 2, createObject("Integer", createIntegerPayload(4)));
-     insertIntoPriorityQueue(queue, 5, createObject("Point", createPointPayload(-4, 8)));
-     insertIntoPriorityQueue(queue, 8, createObject("LocalConditions", createLocalConditionsPayload(7, -5, 10, 12.8)));
-     insertIntoPriorityQueue(queue, 2, createObject("Integer", createIntegerPayload(-4)));
-     insertIntoPriorityQueue(queue, 5, createObject("Decimal", createDecimalPayload(4.5567)));
+    insertIntoPriorityQueue(queue, 2, createObject("Integer", createIntegerPayload(4)));
+    insertIntoPriorityQueue(queue, 5, createObject("Point", createPointPayload(-4, 8)));
+    insertIntoPriorityQueue(queue, 8, createObject("LocalConditions", createLocalConditionsPayload(7, -5, 10, 12.8)));
+    insertIntoPriorityQueue(queue, 2, createObject("Integer", createIntegerPayload(-4)));
+    insertIntoPriorityQueue(queue, 5, createObject("Decimal", createDecimalPayload(4.5567)));
 
-     // the remove
+    // then remove
 
-     Object* firstRemovedObject = removeFromPriorityQueue(queue);
-     LocalConditions* firstRemovedObjectPayload = static_cast<LocalConditions*>(firstRemovedObject->payload);
-     QVERIFY2(strcmp(firstRemovedObject->type, "LocalConditions") == 0 &&
-              firstRemovedObjectPayload->position->x == 7 &&
-              firstRemovedObjectPayload->position->y == -5 &&
-              firstRemovedObjectPayload->temperature == 10 &&
-              firstRemovedObjectPayload->humidity == 12.8,    "Object has not been correctly removed from queue");
+    Object* firstRemovedObject = removeFromPriorityQueue(queue);
+    LocalConditions* firstRemovedObjectPayload = static_cast<LocalConditions*>(firstRemovedObject->payload);
+    QVERIFY2(strcmp(firstRemovedObject->type, "LocalConditions") == 0 &&
+             firstRemovedObjectPayload->position->x == 7 &&
+             firstRemovedObjectPayload->position->y == -5 &&
+             firstRemovedObjectPayload->temperature == 10 &&
+             firstRemovedObjectPayload->humidity == 12.8,    "Object has not been correctly removed from queue");
 
-     Object* secondRemovedObject = removeFromPriorityQueue(queue);
-     Point* secondRemovedObjectPayload = static_cast<Point*>(secondRemovedObject->payload);
-     QVERIFY2(strcmp(secondRemovedObject->type, "Point") == 0 &&
-              secondRemovedObjectPayload->x == -4 &&
-              secondRemovedObjectPayload->y == 8 ,            "Object has not been correctly removed from queue");
+    Object* secondRemovedObject = removeFromPriorityQueue(queue);
+    Point* secondRemovedObjectPayload = static_cast<Point*>(secondRemovedObject->payload);
+    QVERIFY2(strcmp(secondRemovedObject->type, "Point") == 0 &&
+             secondRemovedObjectPayload->x == -4 &&
+             secondRemovedObjectPayload->y == 8 ,            "Object has not been correctly removed from queue");
 
-     Object* thirdRemovedObject = removeFromPriorityQueue(queue);
-     double* thirdRemovedObjectPayload = static_cast<double*>(thirdRemovedObject->payload);
-     QVERIFY2(strcmp(thirdRemovedObject->type, "Decimal") == 0 &&
-              *thirdRemovedObjectPayload == 4.5567,           "Object has not been correctly removed from queue");
+    Object* thirdRemovedObject = removeFromPriorityQueue(queue);
+    double* thirdRemovedObjectPayload = static_cast<double*>(thirdRemovedObject->payload);
+    QVERIFY2(strcmp(thirdRemovedObject->type, "Decimal") == 0 &&
+             *thirdRemovedObjectPayload == 4.5567,           "Object has not been correctly removed from queue");
 
-     Object* fourthRemovedObject = removeFromPriorityQueue(queue);
-     Point* fourthRemovedObjectPayload = static_cast<Point*>(fourthRemovedObject->payload);
-     QVERIFY2(strcmp(secondRemovedObject->type, "Point") == 0 &&
-              fourthRemovedObjectPayload->x == 4 &&
-              fourthRemovedObjectPayload->y == 5 ,            "Object has not been correctly removed from queue");
+    Object* fourthRemovedObject = removeFromPriorityQueue(queue);
+    Point* fourthRemovedObjectPayload = static_cast<Point*>(fourthRemovedObject->payload);
+    QVERIFY2(strcmp(secondRemovedObject->type, "Point") == 0 &&
+             fourthRemovedObjectPayload->x == 4 &&
+             fourthRemovedObjectPayload->y == 5 ,            "Object has not been correctly removed from queue");
 
-     Object* fifthRemovedObject = removeFromPriorityQueue(queue);
-     int* fifthRemovedObjectPayload = static_cast<int*>(fifthRemovedObject->payload);
-     QVERIFY2(strcmp(fifthRemovedObject->type, "Integer") == 0 &&
-              *fifthRemovedObjectPayload == 4,           "Object has not been correctly removed from queue");
-     QVERIFY(!isEmptyQueue(queue));
+    Object* fifthRemovedObject = removeFromPriorityQueue(queue);
+    int* fifthRemovedObjectPayload = static_cast<int*>(fifthRemovedObject->payload);
+    QVERIFY2(strcmp(fifthRemovedObject->type, "Integer") == 0 &&
+             *fifthRemovedObjectPayload == 4,           "Object has not been correctly removed from queue");
+    QVERIFY(!isEmptyQueue(queue));
 
-     Object* sixthRemovedObject = removeFromPriorityQueue(queue);
-     int* sixthRemovedObjectPayload = static_cast<int*>(sixthRemovedObject->payload);
-     QVERIFY2(strcmp(sixthRemovedObject->type, "Integer") == 0 &&
-              *sixthRemovedObjectPayload == -4,           "Object has not been correctly removed from queue");
-     QVERIFY(isEmptyQueue(queue));
+    Object* sixthRemovedObject = removeFromPriorityQueue(queue);
+    int* sixthRemovedObjectPayload = static_cast<int*>(sixthRemovedObject->payload);
+    QVERIFY2(strcmp(sixthRemovedObject->type, "Integer") == 0 &&
+             *sixthRemovedObjectPayload == -4,           "Object has not been correctly removed from queue");
+    QVERIFY(isEmptyQueue(queue));
 
-     deleteTestObject(firstRemovedObject);
-     firstRemovedObject = nullptr;
-     deleteTestObject(secondRemovedObject);
-     secondRemovedObject = nullptr;
-     deleteTestObject(thirdRemovedObject);
-     thirdRemovedObject = nullptr;
-     deleteTestObject(fourthRemovedObject);
-     fourthRemovedObject = nullptr;
-     deleteTestObject(fifthRemovedObject);
-     fifthRemovedObject = nullptr;
-     deleteTestObject(sixthRemovedObject);
-     sixthRemovedObject = nullptr;
-     deletePriorityQueue(queue, deleteTestObject);
-     queue = nullptr;
+    deleteTestObject(firstRemovedObject);
+    firstRemovedObject = nullptr;
+    deleteTestObject(secondRemovedObject);
+    secondRemovedObject = nullptr;
+    deleteTestObject(thirdRemovedObject);
+    thirdRemovedObject = nullptr;
+    deleteTestObject(fourthRemovedObject);
+    fourthRemovedObject = nullptr;
+    deleteTestObject(fifthRemovedObject);
+    fifthRemovedObject = nullptr;
+    deleteTestObject(sixthRemovedObject);
+    sixthRemovedObject = nullptr;
+    deletePriorityQueue(queue, deleteTestObject);
+    queue = nullptr;
 }
 
 void PriorityQueueTests::testClearPriorityQueue()
