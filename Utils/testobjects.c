5943314a5909553e0ee86844279c3f00da4b0276
Liviu Popa <liviucst@gmail.com>
2025-08-02 17:23:34 +0300

    Refactored and updated test objects
    It was required to:
    - solve potential memory leak for LocalConditions
    - remove the result variable in all functions as it was redundant
    - remove unnecessary includes

--- Utils/testobjects.c
+++ Utils/testobjects.c
@@ -1,55 +1,46 @@
-#include <string.h>
-
 #include "testobjects.h"
-#include "error.h"
 
 int* createIntegerPayload(int value)
 {
-    int* result = NULL;
-
     int* integer = (int*)malloc(sizeof(int));
+
     if (integer != NULL)
     {
         *integer = value;
-        result = integer;
     }
 
-    return result;
+    return integer;
 }
 
 double* createDecimalPayload(double value)
 {
-    double* result = NULL;
-
     double* decimal = (double*)malloc(sizeof(double));
+
     if (decimal != NULL)
     {
         *decimal = value;
-        result = decimal;
     }
 
-    return result;
+    return decimal;
 }
+
 Point* createPointPayload(int x, int y)
 {
-    Point* result = NULL;
-
     Point* point = (Point*)malloc(sizeof(Point));
+
     if (point != NULL)
     {
         point->x = x;
         point->y = y;
-        result = point;
     }
 
-    return result;
+    return point;
 }
 
 Segment* createSegmentPayload(int startX, int startY, int stopX, int stopY)
 {
-    Segment* result = NULL;
-
     Segment* segment = (Segment*)malloc(sizeof(Segment));
+
     if (segment != NULL)
     {
         segment->start = (Point*)malloc(sizeof(Point));
@@ -61,7 +52,6 @@ Segment* createSegmentPayload(int startX, int startY, int stopX, int stopY)
             segment->start->y = startY;
             segment->stop->x = stopX;
             segment->stop->y = stopY;
-            result = segment;
         }
         else
         {
@@ -74,32 +64,37 @@ Segment* createSegmentPayload(int startX, int startY, int stopX, int stopY)
         }
     }
 
-    return result;
+    return segment;
 }
 
 LocalConditions* createLocalConditionsPayload(int positionX, int positionY, int temperature, double humidity)
 {
-    LocalConditions* result = NULL;
+    LocalConditions* conditions = NULL;
 
     if (humidity >= 0.0 && humidity <= 100.0)
     {
-        LocalConditions* conditions = (LocalConditions*)malloc(sizeof(LocalConditions));
+        conditions = (LocalConditions*)malloc(sizeof(LocalConditions));
 
         if (conditions != NULL)
         {
             conditions->position = (Point*)malloc(sizeof(Point));
+
             if (conditions->position != NULL)
             {
                 conditions->position->x = positionX;
                 conditions->position->y = positionY;
                 conditions->temperature = temperature;
                 conditions->humidity = humidity;
-                result = conditions;
+            }
+            else
+            {
+                free(conditions);
+                conditions = NULL;
             }
         }
     }
 
-    return result;
+    return conditions;
 }
 
 void emptyTestObject(Object* object)
